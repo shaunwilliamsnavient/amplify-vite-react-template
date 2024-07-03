93646fa116fdcc3ed086cadf8315a4befb8f427e
Williams <Shaun.Williams@navient.com>
2024-07-03 09:14:09 -0400

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -18,8 +18,6 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
