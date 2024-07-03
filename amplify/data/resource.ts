de34cea6602ba5c4923b14de36887822d4028625
Williams <Shaun.Williams@navient.com>
2024-07-03 08:59:53 -0400

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,6 +10,7 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+	 }).authorization(allow => [allow.owner()]),  
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -24,6 +25,7 @@ export const data = defineData({
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
+	defaultAuthorizationMode: 'userPool',
   },
 });
 
